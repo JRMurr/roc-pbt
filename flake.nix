b311421e2c6d12e7bbb376d8263ce8b35a93472e
John Murray <5672686+JRMurr@users.noreply.github.com>
2023-12-14 20:59:26 -0500

    add rust

--- flake.nix
+++ flake.nix
@@ -3,16 +3,36 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     roc.url = "github:roc-lang/roc";
+    roc2nix = {
+      url = "github:JRMurr/roc2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.roc.follows = "roc";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, roc, ... }:
+  outputs = { self, nixpkgs, flake-utils, roc, roc2nix, rust-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
 
         rocPkgs = roc.packages.${system};
-
         rocFull = rocPkgs.full;
+        rocLib = (roc2nix.lib.${system}).overrideToolchain rocFull;
+
+        rustVersion = (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml);
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rustVersion;
+          rustc = rustVersion;
+        };
+
+        rust-shell =
+          (rustVersion.override { extensions = [ "rust-src" "rust-analyzer" ]; });
 
       in
       {
@@ -22,10 +42,10 @@
           default = pkgs.mkShell {
             buildInputs = with pkgs;
               [
-                rocFull # cli included in here
+                rocFull
+                rust-shell
               ];
 
-            # ROC_LSP_PATH will be read by https://github.com/ivan-demchenko/roc-vscode-unofficial
             shellHook = ''
               export ROC_LSP_PATH=${rocFull}/bin/roc_ls
             '';
